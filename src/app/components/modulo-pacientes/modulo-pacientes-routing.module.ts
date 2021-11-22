a4c9fcc30920a13b4670c56303b8fbed8d26246e
leonardo20002 <lrodriguezp0522@gmail.com>
2021-11-22 17:24:01 -0600

    Update modulo-pacientes-routing.module.ts

--- src/app/components/modulo-pacientes/modulo-pacientes-routing.module.ts
+++ src/app/components/modulo-pacientes/modulo-pacientes-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioPacientesComponent } from './inicio-pacientes/inicio-pacientes.component';
 import { ModuloPacientesComponent } from './modulo-pacientes.component';
 
 const routes: Routes = [
   {path:'', component: ModuloPacientesComponent,children:[
-    
+   {path:'', component:InicioPacientesComponent},
+   {}
   ]}
 ];
 
