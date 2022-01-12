2eee17f8b82e5dcf35b3563841179808e05ad836
leonardo20002 <lrodriguezp0522@gmail.com>
2022-01-12 00:37:03 -0600

    Actualización a inicio de pacientes

--- src/app/components/modulo-pacientes/inicio-pacientes/inicio-pacientes.component.ts
+++ src/app/components/modulo-pacientes/inicio-pacientes/inicio-pacientes.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class InicioPacientesComponent implements OnInit {
 
   constructor() { }
-
+  usuario = localStorage.getItem('usuario');
   ngOnInit(): void {
   }
 
