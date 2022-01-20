8f619d880afc290c6dbe241bd7c53edc54b2079c
HECTOR JOSUE GONZALEZ CORTES <62352013+iHector1@users.noreply.github.com>
2022-01-20 12:33:58 -0600

    ya sep ueden ver los pacientes

--- src/app/components/modulo-especialistas/revisar-paciente/revisar-paciente.component.ts
+++ src/app/components/modulo-especialistas/revisar-paciente/revisar-paciente.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServiceRevisarPacienteService } from './service-revisar-paciente.service';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import {Pacientes} from 'src/app/interfaces/usuario';
+import { Pacientes } from 'src/app/interfaces/usuario';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -19,9 +19,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class RevisarPacienteComponent implements OnInit {
 
-  lista_pacientes:Pacientes[] = []
+  lista_pacientes: any = []
 
-  displayedColumns: string[] = ['usuario', 'nombre', 'nacimiento', 'sexo','acciones'];
+  displayedColumns: string[] = ['usuario', 'nombre', 'nacimiento', 'sexo', 'precio', 'acciones'];
   dataSource = new MatTableDataSource(this.lista_pacientes);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -37,19 +37,26 @@ export class RevisarPacienteComponent implements OnInit {
   }
 
 
-  constructor(private verPacientesService: ServiceRevisarPacienteService, private router: Router, private _snackBar:MatSnackBar) { }
+  constructor(private verPacientesService: ServiceRevisarPacienteService, private router: Router, private _snackBar: MatSnackBar) {
+  }
 
   id_usuario = localStorage.getItem('id_usuario');
 
   ngOnInit(): void {
-    this.verPacientes();
+    // this.verPacientes();
+    this.id_usuario = localStorage.getItem('id_usuario');
     this.cargarPacientes();
   }
-  cargarPacientes(){
-    this.lista_pacientes= this.verPacientesService.getPaciente();
-    this.dataSource = new MatTableDataSource(this.lista_pacientes);
+  cargarPacientes() {
+    this.verPacientesService.getPacientes().subscribe(res => {
+      this.lista_pacientes = res;
+      console.log(this.lista_pacientes);
+      this.dataSource = new MatTableDataSource(this.lista_pacientes);
+    }, err => console.log(err)
+    );
   }
-  verPacientes(){
+  /*
+  verPacientes() {
 
     this.verPacientesService.Pacientes(this.id_usuario).subscribe(
       res => {
@@ -59,16 +66,16 @@ export class RevisarPacienteComponent implements OnInit {
         console.log(err);
       }
     )
-  }
-  eliminarPaciente(index:number){//aqui lo que abria que pasarle es el id, para borra el dato de la base de datos.
+  }*/
+  eliminarPaciente(index: number) {//aqui lo que abria que pasarle es el id, para borra el dato de la base de datos.
     console.log(index);
     this.verPacientesService.eliminarPaciente(index);
     this.cargarPacientes();
 
-    this._snackBar.open('El usuario fue eliminado con exito', '',{
-      duration:1500,
-      horizontalPosition:'center',
-      verticalPosition:'bottom'
+    this._snackBar.open('El usuario fue eliminado con exito', '', {
+      duration: 1500,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
     })
   }
   ///this.reportesService.replicarReporte(this.replicaId, this.registrarForm?.value).subscribe(
--- src/app/components/modulo-especialistas/revisar-paciente/service-revisar-paciente.service.ts
+++ src/app/components/modulo-especialistas/revisar-paciente/service-revisar-paciente.service.ts
@@ -7,25 +7,28 @@ import { Pacientes } from 'src/app/interfaces/usuario';
 })
 export class ServiceRevisarPacienteService {
   lista_pacientes: Pacientes[] = [
-    {usuario:"Leonard222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Leonard222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Leonard222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Leonard222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Leonard222", nombre:"Juan", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Perez222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
-    {usuario:"Leonard222", nombre:"Leonardo", nacimiento:"22/05/2002",sexo:"Hombre"},
+    { usuario: "Leonard222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Leonard222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Leonard222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Leonard222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Leonard222", nombre: "Juan", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Perez222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
+    { usuario: "Leonard222", nombre: "Leonardo", nacimiento: "22/05/2002", sexo: "Hombre" },
   ];
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  public Pacientes(id_usuario?: string | null){
-    return this.http.get<any>('http://localhost:4000/ver-pacientes/' + id_usuario);
+  public Pacientes(id_usuario?: string | null) {
+    return this.http.get<any>('http://localhost:4000/Pacientes/' + id_usuario);
   }
-  getPaciente(){
+  getPaciente() {
     return this.lista_pacientes.slice();//slice retorna una copia del estado de usuarios
   }
 
-  eliminarPaciente(index:number){
-    this.lista_pacientes.splice(index,1);//hay que pasarle que elemento quiero eliminar, y cuantos
+  eliminarPaciente(index: number) {
+    this.lista_pacientes.splice(index, 1);//hay que pasarle que elemento quiero eliminar, y cuantos
 
+  }
+  getPacientes() {
+    return this.http.get('http://localhost:4000/Pacientes/');
   }
 }
