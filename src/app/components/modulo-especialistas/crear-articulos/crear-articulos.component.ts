dbd97fd47049c8562e1e447ea2560747e0b619b8
leonardo20002 <lrodriguezp0522@gmail.com>
2022-01-21 23:56:49 -0600

    ya funciona el editor de texto

--- src/app/components/modulo-especialistas/crear-articulos/crear-articulos.component.ts
+++ src/app/components/modulo-especialistas/crear-articulos/crear-articulos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-crear-articulos',
@@ -11,5 +12,54 @@ export class CrearArticulosComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+      spellcheck: true,
+      height: 'auto',
+      minHeight: '0',
+      maxHeight: 'auto',
+      width: 'auto',
+      minWidth: '0',
+      translate: 'yes',
+      enableToolbar: true,
+      showToolbar: true,
+      placeholder: 'Enter text here...',
+      defaultParagraphSeparator: '',
+      defaultFontName: '',
+      defaultFontSize: '',
+      fonts: [
+        {class: 'arial', name: 'Arial'},
+        {class: 'times-new-roman', name: 'Times New Roman'},
+        {class: 'calibri', name: 'Calibri'},
+        {class: 'comic-sans-ms', name: 'Comic Sans MS'}
+      ],
+      customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+    //no se bien que onda con eso
+    /* 
+    uploadUrl: 'v1/image',
+    upload: (file: File) => { ... }
+    uploadWithCredentials: false,
+    */
+    sanitize: true,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [
+      ['InsertVideo', ''],
+      ['']
+    ]
+};
 
 }
