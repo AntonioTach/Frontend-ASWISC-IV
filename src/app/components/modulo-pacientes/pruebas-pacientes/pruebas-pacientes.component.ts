e32865e2f68854e1ef94ad7bd569e014632cefbd
HECTOR JOSUE GONZALEZ CORTES <62352013+iHector1@users.noreply.github.com>
2022-01-25 10:04:24 -0600

    se pueden ver todas las pruebas en paciente

--- src/app/components/modulo-pacientes/pruebas-pacientes/pruebas-pacientes.component.ts
+++ src/app/components/modulo-pacientes/pruebas-pacientes/pruebas-pacientes.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { PruebasService } from './pruebas.service';
 
 @Component({
   selector: 'app-pruebas-pacientes',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pruebas-pacientes.component.css']
 })
 export class PruebasPacientesComponent implements OnInit {
+  pruebas: any = []
 
-  constructor() { }
-
-  ngOnInit(): void {
+  displayedColumns: string[] = ['titulo', 'descargar'];
+  dataSource = new MatTableDataSource(this.pruebas);
+  id_usuario = localStorage.getItem('id_usuario');
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  constructor(private pruebasService: PruebasService) { }
+
+  ngOnInit(): void {
+    this.pruebasService.getPruebas(this.id_usuario.toString()).subscribe(res => {
+      this.pruebas = res;
+      console.log(this.pruebas);
+      this.dataSource = new MatTableDataSource(this.pruebas);
+    }, err => { console.log(err); })
+  }
+  verPrueba(id_prueba) {
+    console.log(id_prueba);
+  }
 }
---
+++ src/app/components/modulo-pacientes/pruebas-pacientes/pruebas.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class PruebasService {
+
+  constructor(private http: HttpClient) { }
+
+  getPruebas(id: any) {
+    return this.http.get('http://localhost:4000/pruebas-paciente/' + id)
+  }
+}
