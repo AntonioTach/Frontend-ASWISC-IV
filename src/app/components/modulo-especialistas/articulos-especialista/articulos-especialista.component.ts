b604f13d1275722695b1b2464f62e36cc780cee5
AntonioTach <antach.vihe@gmail.com>
2022-01-27 15:13:49 -0600

    reqs

--- src/app/components/modulo-especialistas/articulos-especialista/articulos-especialista.component.ts
+++ src/app/components/modulo-especialistas/articulos-especialista/articulos-especialista.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { articulo } from 'src/app/interfaces/articulo';
 import { Router, RouterLink } from '@angular/router';
 import { ArticulosService } from '../crear-articulos/articulos.service';
-
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 
@@ -12,20 +13,23 @@ import { ArticulosService } from '../crear-articulos/articulos.service';
   styleUrls: ['./articulos-especialista.component.css']
 })
 export class ArticulosEspecialistaComponent implements OnInit {
-
+  lista: any = []
   displayedColumns: string[] = ['titulo', 'nombre', 'fecha', 'acciones'];//columnas
-  dataSource: any = [];
+  dataSource = new MatTableDataSource(this.lista);
 
-
+  @ViewChild(MatSort) sort!: MatSort;
   applyFilter(event: Event) {
-
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   constructor(private router: Router, private articulos: ArticulosService) { }
 
   ngOnInit(): void {
     this.articulos.getArticulos().subscribe(res => {
-      this.dataSource = res;
+      this.lista = res;
+      this.dataSource = new MatTableDataSource(this.lista);
+      // this.dataSource = res;
     })
   }
 
