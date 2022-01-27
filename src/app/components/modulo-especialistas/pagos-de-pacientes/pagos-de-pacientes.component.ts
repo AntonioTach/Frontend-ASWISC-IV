8fe22e4e0e181ceb2295703140cae57e309fae40
leonardo20002 <lrodriguezp0522@gmail.com>
2022-01-27 12:48:30 -0600

    Revert "inicio pagos de pacientes en especialistas"
    This reverts commit 199c0326c90168772a8c6c469e62b2d6bf1be71c.

--- src/app/components/modulo-especialistas/pagos-de-pacientes/pagos-de-pacientes.component.ts
+++ src/app/components/modulo-especialistas/pagos-de-pacientes/pagos-de-pacientes.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { pagos } from 'src/app/interfaces/pagos';
-import { render } from 'creditcardpayments/creditCardPayments';
-import { PagosDePacientesService } from './pagos-de-pacientes.service';
 
 
-// const ELEMENT_DATA: pagos[] = [//array de elementos
-//   {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
-//   {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
-//   {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
+const ELEMENT_DATA: pagos[] = [//array de elementos
+  {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
+  {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
+  {nombre:'Hydrogen', monto: 'Hydrogen', fecha: 'Hydrogen'},
   
-// ];
+];
 @Component({
   selector: 'app-pagos-de-pacientes',
   templateUrl: './pagos-de-pacientes.component.html',
@@ -18,64 +16,17 @@ import { PagosDePacientesService } from './pagos-de-pacientes.service';
 })
 export class PagosDePacientesComponent implements OnInit {
 
-  // displayedColumns: string[] = ['nombre', 'monto', 'fecha'];//columnas
-  // dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['nombre', 'monto', 'fecha'];//columnas
+  dataSource = ELEMENT_DATA;
 
-  id: any;
-  pagos: any;
-  total: any = 1;
-
-  constructor(private carrito: PagosDePacientesService) {
-    this.carrito.getPagos().subscribe((res: any) => {
-      this.pagos = res;
-      if (res.length == 0) {
-        this.id = 0
-      } else {
-        this.id = res[0].id_paciente;
-      }
-      console.log(this.pagos);
-      for (let i = 0; i < this.pagos.length; i++) {
-        this.total = this.total + this.pagos[i].precio;
-        console.log(this.pagos[i].precio);
-      }
-      this.total = this.total -1;
-      console.log(this.total);
-      render(
-        {
-          id: "#payments",
-          currency: "MXN",
-          value: this.total,
-          onApprove: (details) => {
-            this.carrito.subirPagos(this.id).subscribe((res) => {
-              console.log(res);
-            })
-            console.log(details);
-          }
-        }
-      )
-    }, err => {
-      render(
-        {
-          id: "#payments",
-          currency: "MXN",
-          value: "1",
-          onApprove: (details) => {
-            this.carrito.subirPagos(this.id).subscribe((res) => {
-              console.log(res);
-            })
-            console.log(details);
-          }
-        }
-      )
-    })
-  }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
-  // agregartarjeta(){
-  //   console.log('Agregar Tarjeta');
-  //   this.router.navigateByUrl('/modulo-especialistas/tarjeta');
-  // }
+  agregartarjeta(){
+    console.log('Agregar Tarjeta');
+    this.router.navigateByUrl('/modulo-especialistas/tarjeta');
+  }
 
 }
