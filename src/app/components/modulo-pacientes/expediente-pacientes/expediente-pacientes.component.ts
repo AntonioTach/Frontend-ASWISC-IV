4f1e9e969df6d7f710f8a961c445fbe27de37ece
Angel <angelrivas0day@gmail.com>
2022-01-27 16:13:17 -0600

    Expediente desde paciente

--- src/app/components/modulo-pacientes/expediente-pacientes/expediente-pacientes.component.ts
+++ src/app/components/modulo-pacientes/expediente-pacientes/expediente-pacientes.component.ts
@@ -3,6 +3,8 @@ import * as jspdf from 'jspdf-autotable';
 import jsPDF from 'jspdf'
 import * as html2pdf from 'html2pdf.js'
 import html2canvas from 'html2canvas'
+import { ExpedientePacientesService } from './expediente-pacientes.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-expediente-pacientes',
@@ -11,9 +13,57 @@ import html2canvas from 'html2canvas'
 })
 export class ExpedientePacientesComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup = new FormGroup({
+      usuario: new FormControl({value: '', disabled: true}, [Validators.required]),
+      telefono: new FormControl({value: '', disabled: true}, [Validators.required]),
+      nacimiento: new FormControl({value: '', disabled: true}, [Validators.required]),
+      email: new FormControl({value: '', disabled: true}, [Validators.required]),
+      sexo: new FormControl({value: '', disabled: true}, [Validators.required]),
+      estudios: new FormControl({value: '', disabled: true}, [Validators.required]),
+      origen: new FormControl({value: '', disabled: true}, [Validators.required]),
+      ocupacion: new FormControl({value: '', disabled: true}, [Validators.required]),
+      obsevacion: new FormControl({value: '', disabled: true}, [Validators.required]),
+  })
+
+  constructor(
+    private expedientePacientesService: ExpedientePacientesService
+  ) { }
 
   ngOnInit(): void {
+    this.traerPaciente()
+  }
+
+  traerPaciente(): void {
+    let id = localStorage.getItem('id_usuario')
+    this.expedientePacientesService.getPaciente(id)
+    .subscribe((res:any) => {
+      console.log('resp: ', res)
+      this.formulario.patchValue(res[0])
+    })
+  }
+
+  capturar(): void {
+    // Extraemos el
+    console.log('si sirvo');
+    const DATA = document.getElementById('registro');
+    const doc = new jsPDF('p', 'pt', 'a2');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+      const img = canvas.toDataURL('image/PNG');
+      // Add image Canvas to PDF
+      const bufferX = 40;
+      const bufferY = 40;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${this.formulario.get('usuario').value}.pdf`);
+    });
   }
 
 }
