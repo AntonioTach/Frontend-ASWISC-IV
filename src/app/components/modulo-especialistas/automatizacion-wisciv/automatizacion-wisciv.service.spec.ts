f23e8287f2ab469463f1f654b2ea3b280fde8be0
AntonioTach <antach.vihe@gmail.com>
2022-01-27 22:33:24 -0600

    leve front aswisc

---
+++ src/app/components/modulo-especialistas/automatizacion-wisciv/automatizacion-wisciv.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { AutomatizacionWiscivService } from './automatizacion-wisciv.service';
+
+describe('Service: AutomatizacionWisciv', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AutomatizacionWiscivService]
+    });
+  });
+
+  it('should ...', inject([AutomatizacionWiscivService], (service: AutomatizacionWiscivService) => {
+    expect(service).toBeTruthy();
+  }));
+});
