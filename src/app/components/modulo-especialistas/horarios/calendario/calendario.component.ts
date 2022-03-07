88f5d49b80a3a1ee94f4d8ea98694ddd1917e7df
AntonioTach <antach.vihe@gmail.com>
2022-03-06 18:11:33 -0600

    Al instalar dependencias de horarios, el proyecto no funciona

--- src/app/components/modulo-especialistas/horarios/calendario/calendario.component.ts
+++ src/app/components/modulo-especialistas/horarios/calendario/calendario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
+//import { EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
 import * as moment from 'moment';
 
 @Component({
@@ -10,45 +10,45 @@ import * as moment from 'moment';
 export class CalendarioComponent {
 
 
-  public eventData: EventSettingsModel = {
+  // public eventData: EventSettingsModel = {
 
-    dataSource: [{
+  //   dataSource: [{
 
-      Id: 1,
+  //     Id: 1,
 
-      Subject: 'Board Meeting',
+  //     Subject: 'Board Meeting',
 
-      StartTime: new Date(2022, 0, 26, 3, 24, 0),
+  //     StartTime: new Date(2022, 0, 26, 3, 24, 0),
 
-      EndTime: new Date(2022, 0, 26, 4, 24, 0),
-      description: "holasd"
+  //     EndTime: new Date(2022, 0, 26, 4, 24, 0),
+  //     description: "holasd"
 
-    },
+  //   },
 
-    {
+  //   {
 
-      Id: 2,
+  //     Id: 2,
 
-      Subject: 'Training session on JSP',
+  //     Subject: 'Training session on JSP',
 
-      StartTime: new Date(),
+  //     StartTime: new Date(),
 
-      EndTime: new Date(1643236363)
+  //     EndTime: new Date(1643236363)
 
-    },
+  //   },
 
-    {
+  //   {
 
-      Id: 3,
+  //     Id: 3,
 
-      Subject: 'Sprint Planning with Team members',
+  //     Subject: 'Sprint Planning with Team members',
 
-      StartTime: new Date(2022, 10, 30, 9, 30),
+  //     StartTime: new Date(2022, 10, 30, 9, 30),
 
-      EndTime: new Date(2022, 10, 30, 11, 0)
+  //     EndTime: new Date(2022, 10, 30, 11, 0)
 
-    }]
+  //   }]
 
-  }
+  // }
 
 }
