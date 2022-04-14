dcade7c44c027656cb674ce9326eb3e6a6ea1502
Saul Izcali <saulizcali@gmail.com>
2022-04-14 14:54:56 -0500

    form full days p2

--- src/app/components/modulo-especialistas/horarios/calendario/calendario.component.ts
+++ src/app/components/modulo-especialistas/horarios/calendario/calendario.component.ts
@@ -20,8 +20,9 @@ import { HorariosServiceService } from "../../../../services/horarios/horarios-s
   providers: [MonthService, DayService, WeekService, WorkWeekService, ResizeService, DragAndDropService],
 })
 export class CalendarioComponent implements OnInit{  
+  
   @ViewChild("scheduleObj", { static: false })
-  date = new Date()
+  p = "s"
   
   constructor(public horariosServiceService:HorariosServiceService){}
 
@@ -43,7 +44,7 @@ export class CalendarioComponent implements OnInit{
   };
 
 
-  public data: object [] = [
+  public data: object [] =[
     {
       Id: 1, // esta cita es para que no se pueda agendar en horas que ya pasaron
       eventName: '',
@@ -61,8 +62,10 @@ export class CalendarioComponent implements OnInit{
       isAllDay: false,
       color: "#d6d6d6",
       OwnerId: 1
-    },
+    }, 
   ];
+  
+
          
   
   public eventSettings: EventSettingsModel = {
@@ -79,52 +82,15 @@ export class CalendarioComponent implements OnInit{
 
 
   ngOnInit(): void {
-    let year = this.date.getFullYear();
-    let month = this.date.getMonth();
-    let day = this.date.getDate();
+    // this.data = this.dataAux
+    this.modifyFullDaysData();
+
 
     this.horariosServiceService.TriggerFullDays.subscribe(res => {
         // console.log(res)
 
-        let today = Date.parse(year + "-" + month + "-" + day) - 3600000
-        console.log(today)  // obtiene la fecha en mlseg al momento en que empezo el dia de hoy
+        this.modifyFullDaysData();
 
-        // let fechaHoy = new Date(year, month, day, 0, 0) 
-        // console.log(fechaHoy)
-
-        let lastDayOfYear = Date.parse("2022-11-31");
-
-        let dayMilliseconds = 86400000;
-        let diff_in_millisenconds = lastDayOfYear - today;
-        let daysToFinishYear = diff_in_millisenconds / dayMilliseconds;
-
-        console.log( daysToFinishYear );
-
-        let weekCounter = 0, changingDay = today
-
-        for (let i = 4; i < daysToFinishYear + 4; i++) {
-          weekCounter++
-          if(weekCounter % 7 == 0){
-              changingDay = changingDay + (dayMilliseconds * 7)
-              // console.log(new Date(changingDay))
-              // console.log( weekCounter + 4)
-
-              let auxObj = {
-                id: i ,
-                eventName: '',
-                startTime: new Date(changingDay),
-                endTime: new Date(changingDay + dayMilliseconds),
-                isAllDay: false,
-                color: "#e49898",
-                IsBlock: true,
-              }
-              
-              this.data.push(auxObj)
-          }
-
-        }
-
-        console.log(this.data)
       }, err => {
         console.error("ocurrio algún error", err)
     })
@@ -132,7 +98,71 @@ export class CalendarioComponent implements OnInit{
   }
 
 
-  
+  modifyFullDaysData(){
+    let date = new Date()
+    let year =  date.getFullYear().toString();
+    let month =  date.getMonth() + 1;
+    let day =  date.getDate();
+    let arrayDaysStatus = [ , , , , , , ]
+
+    console.log(year, month, day)
+    let today = Date.parse(year + "-" + month + "-" + day)  // obtiene la fecha en mlseg al momento en que empezo el dia de hoy
+    // let fechaHoy = new Date(year, month, day, 0, 0) 
+
+    console.log(date.getDay())
+
+    try {
+      // this.data = []
+      // this.data = this.dataAux
+
+      let fullDaysData: any = JSON.parse(localStorage.getItem("fullDaysData"))
+      
+      if(fullDaysData){          // SI SE ENCUENTRA VARIABLE EN EL LS SE HACE TODO EL PROCESO
+        arrayDaysStatus[0] = fullDaysData[6].domingo
+        arrayDaysStatus[1] = fullDaysData[0].lunes
+        arrayDaysStatus[2] = fullDaysData[1].martes
+        arrayDaysStatus[3] = fullDaysData[2].miercoles
+        arrayDaysStatus[4] = fullDaysData[3].jueves
+        arrayDaysStatus[5] = fullDaysData[4].viernes
+        arrayDaysStatus[6] = fullDaysData[5].sabado
+
+          let lastDayOfYear = Date.parse("2022-12-31");
+
+          let dayMilliseconds = 86400000;
+          let diff_in_millisenconds = lastDayOfYear - today;
+          let daysToFinishYear = diff_in_millisenconds / dayMilliseconds;
+      
+          let weekCounter = 0, changingDay = today, dayTour = date.getDay()
+      
+          for (let i = 4; i < daysToFinishYear + 4; i++) {
+              if(arrayDaysStatus[dayTour]){
+
+                  let auxObj = {
+                    id: i ,
+                    eventName: '',
+                    startTime: new Date(changingDay ),
+                    endTime: new Date(changingDay + dayMilliseconds - 3600000),
+                    isAllDay: false,
+                    color: "#e49898",
+                    IsBlock: true,
+                  }
+                    
+                  this.data.push(auxObj)
+              }
+              
+              changingDay = changingDay + (dayMilliseconds)
+              
+              dayTour++
+              if(dayTour == 7) dayTour = 0
+          }
+      
+          console.log(this.data)
+      }
+    } catch (error) {
+      console.error("Aun no se ha configurado los horarios", error);
+    }
+
+  }
 
 
   onRenderCell(args: RenderCellEventArgs): void {
